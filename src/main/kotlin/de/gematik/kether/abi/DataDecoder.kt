001950db51f998c206e114dfda52846b3584a81b
rk <ronald.koenig@gematik.de>
2022-09-28 16:27:57 +0200

    improved handling of decoding error

--- src/main/kotlin/de/gematik/kether/abi/DataDecoder.kt
+++ src/main/kotlin/de/gematik/kether/abi/DataDecoder.kt
@@ -14,26 +14,26 @@ class DataDecoder(val data: Data) {
     inline fun <reified T> next(): T {
         return when (T::class) {
             AbiSelector::class -> {
-                check(pos == 0 && data.toByteArray().size >=4) {"data decoding error: remaining data too short (pos: $pos, limit: ${data.toByteArray().size}, type: ${T::class.simpleName}"}
+                check(pos == 0 && data.toByteArray().size >=4) {"data decoding error: remaining data too short (pos: $pos, limit: ${data.toByteArray().size}, type: ${T::class.simpleName})"}
                 val bytes = data.toByteArray().copyOfRange(0, 4)
                 pos+=4
                 AbiSelector(bytes) as T
             }
             AbiUint256::class -> {
-                check(data.toByteArray().size - pos >= 32) {"data decoding error: remaining data too short (pos: $pos, limit: ${data.toByteArray().size}, type: ${T::class.simpleName}"}
+                check(data.toByteArray().size - pos >= 32) {"data decoding error: remaining data too short (pos: $pos, limit: ${data.toByteArray().size}, type: ${T::class.simpleName})"}
                 val bytes = data.toByteArray().copyOfRange(pos, pos + 32)
                 pos += 32
                 AbiUint256(bytes) as T
             }
             AbiString::class -> {
-                check(data.toByteArray().size - pos >= 32) {"data decoding error: remaining data too short (pos: $pos, limit: ${data.toByteArray().size}, type: ${T::class.simpleName}"}
+                check(data.toByteArray().size - pos >= 32) {"data decoding error: remaining data too short (pos: $pos, limit: ${data.toByteArray().size}, type: ${T::class.simpleName})"}
                 val offset = BigInteger(data.toByteArray().copyOfRange(pos, pos + 32)).toInt()
                 pos += 32
                 val length = BigInteger(data.toByteArray().copyOfRange(offset, offset + 32)).toInt()
                 String(data.toByteArray().copyOfRange(offset + 32, offset + 32 + length)) as T
             }
             AbiBytes32::class -> {
-                check(data.toByteArray().size - pos >= 32) {"data decoding error: remaining data too short (pos: $pos, limit: ${data.toByteArray().size}, type: ${T::class.simpleName}"}
+                check(data.toByteArray().size - pos >= 32) {"data decoding error: remaining data too short (pos: $pos, limit: ${data.toByteArray().size}, type: ${T::class.simpleName})"}
                 val bytes = data.toByteArray().copyOfRange(pos, pos + 32)
                 pos += 32
                 AbiBytes32(bytes) as T
