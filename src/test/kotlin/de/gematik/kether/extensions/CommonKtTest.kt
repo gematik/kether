602c452c91eced1c8d3471286f1155ae37b3bda4
Thomas Fritz <thomas.fritz@gematik.de>
2023-02-07 11:40:16 +0100

    test

---
+++ src/test/kotlin/de/gematik/kether/extensions/CommonKtTest.kt
@@ -0,0 +1,12 @@
+package de.gematik.kether.extensions
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+class CommonKtTest {
+
+    @Test
+    fun toByteArray() {
+    }
+}
